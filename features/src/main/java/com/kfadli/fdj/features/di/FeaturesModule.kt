d96f1945fd08259641e376a872b233365762b4fc
Karim <karimm.fadli@gmail.Com>
2024-08-26 22:00:43 +0200

    Fix Koin issue

--- features/src/main/java/com/kfadli/fdj/features/di/FeaturesModule.kt
+++ features/src/main/java/com/kfadli/fdj/features/di/FeaturesModule.kt
@@ -1,10 +1,15 @@
 package com.kfadli.fdj.features.di
 
 import com.kfadli.fdj.features.screens.home.HomeViewModel
-import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val featuresModule =
     module {
-        viewModelOf(::HomeViewModel)
+        viewModel {
+            HomeViewModel(
+                getLeaguesUseCase = get(),
+                getTeamsUseCase = get(),
+            )
+        }
     }
