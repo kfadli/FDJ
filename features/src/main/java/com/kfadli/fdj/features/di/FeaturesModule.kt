77b259d31135284ae35e30f1d264a3c8becbe24e
Karim <karimm.fadli@gmail.Com>
2024-08-26 17:21:07 +0200

    Remove unused injection for deleted classe

--- features/src/main/java/com/kfadli/fdj/features/di/FeaturesModule.kt
+++ features/src/main/java/com/kfadli/fdj/features/di/FeaturesModule.kt
@@ -1,6 +1,5 @@
 package com.kfadli.fdj.features.di
 
-import com.kfadli.fdj.features.screens.detail.LeagueDetailViewModel
 import com.kfadli.fdj.features.screens.home.HomeViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
@@ -8,5 +7,4 @@ import org.koin.dsl.module
 val featuresModule =
     module {
         viewModelOf(::HomeViewModel)
-        viewModelOf(::LeagueDetailViewModel)
     }
