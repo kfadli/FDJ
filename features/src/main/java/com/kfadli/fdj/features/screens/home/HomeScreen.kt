830c8bf37e09fc72d1b9c1b6d05f35cc44fc0cef
Karim <karimm.fadli@gmail.Com>
2024-08-26 17:19:03 +0200

    Implements HomeFeature

--- features/src/main/java/com/kfadli/fdj/features/screens/home/HomeScreen.kt
+++ features/src/main/java/com/kfadli/fdj/features/screens/home/HomeScreen.kt
@@ -1,19 +1,31 @@
 package com.kfadli.fdj.features.screens.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.kfadli.fdj.domain.model.LeagueUI
+import androidx.compose.ui.unit.sp
+import com.kfadli.fdj.domain.models.LeagueUI
+import com.kfadli.fdj.domain.models.TeamUI
+import com.kfadli.fdj.features.extensions.toStringError
+import com.kfadli.fdj.features.ui.AutoCompleteSelectBar
+import com.kfadli.fdj.features.ui.BadgeFromUrl
 import com.kfadli.fdj.features.ui.FDJLoadingDialog
 import com.kfadli.fdj.features.ui.FDJPopupDialog
 import org.koin.compose.viewmodel.koinViewModel
@@ -27,45 +39,105 @@ fun HomeScreen(modifier: Modifier) {
         viewModel.getLeagues()
     }
 
-    Box(modifier = modifier) {
-        HomeLayout(state.value)
-    }
+    HomeLayout(
+        modifier = modifier,
+        state = state.value,
+        onSelected = { viewModel.getTeams(it) },
+        onRetry = { viewModel.getLeagues() },
+    )
 }
 
 @Composable
-fun HomeLayout(state: UIState) {
+fun HomeLayout(
+    modifier: Modifier = Modifier,
+    state: UIState,
+    onSelected: (String) -> Unit,
+    onRetry: () -> Unit,
+) {
+    val teams = remember { mutableStateOf<List<TeamUI>?>(null) }
+    val leagues = remember { mutableStateOf(emptyList<LeagueUI>()) }
+
+    var showErrorDialog by remember { mutableStateOf(false) }
+    var messageErrorDialog by remember { mutableStateOf<String?>(null) }
+    var leagueSelected by remember { mutableStateOf("") }
+
     when (state) {
-        is UIState.Failure ->
-            FDJPopupDialog(
-                content = state.cause.message ?: "Unknown error",
-                textSubmit = null,
-                onCancel = {},
-            )
-
+        is UIState.Failure -> {
+            messageErrorDialog = state.cause.toStringError()
+            showErrorDialog = true
+        }
         UIState.Loading -> FDJLoadingDialog()
-        is UIState.Success -> LeagueList(state.items)
+        is UIState.LeagueUISuccess -> leagues.value = state.items
+        is UIState.TeamsUISuccess -> teams.value = state.items
+        else -> {}
     }
-}
 
-@Composable
-fun LeagueList(items: List<LeagueUI>) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 128.dp),
-    ) {
-        items(items) {
-            LeagueItem(it)
+    Column(modifier = modifier) {
+        AutoCompleteSelectBar(
+            entries = leagues.value.map { it.name },
+            hint = "Search by league",
+            label = "Leagues",
+            onSelected = {
+                leagueSelected = it
+                onSelected(it)
+            },
+        )
+
+        teams.value?.let {
+            TeamList(items = it)
+        }
+
+        if (showErrorDialog) {
+            FDJPopupDialog(
+                content = messageErrorDialog ?: "Unknown error",
+                textSubmit = "Retry",
+                onCancel = {
+                    showErrorDialog = false
+                    messageErrorDialog = null
+                },
+                onConfirm =
+                    if (leagues.value.isEmpty()) {
+                        { onRetry() }
+                    } else {
+                        null
+                    },
+            )
         }
     }
 }
 
 @Composable
-fun LeagueItem(league: LeagueUI) {
+fun TeamList(items: List<TeamUI>) {
+    LazyVerticalGrid(
+        modifier = Modifier.padding(16.dp),
+        columns = GridCells.Fixed(count = 2),
+    ) {
+        items(items) {
+            TeamItem(it)
+        }
+    }
+
+    if (items.isEmpty()) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "Teams not found",
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 18.sp,
+        )
+    }
+}
+
+@Composable
+fun TeamItem(team: TeamUI) {
     Column(
         modifier =
             Modifier
+                .testTag("team_${team.id}")
                 .padding(8.dp)
                 .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = league.name)
+        BadgeFromUrl(imageUrl = team.iconUrl, contentDescription = team.name)
     }
 }
--- features/src/main/java/com/kfadli/fdj/features/screens/home/HomeViewModel.kt
+++ features/src/main/java/com/kfadli/fdj/features/screens/home/HomeViewModel.kt
@@ -2,37 +2,66 @@ package com.kfadli.fdj.features.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kfadli.fdj.domain.GetAllLeaguesUseCase
-import com.kfadli.fdj.domain.model.LeagueUI
+import com.kfadli.fdj.domain.models.LeagueUI
+import com.kfadli.fdj.domain.models.TeamUI
+import com.kfadli.fdj.domain.usecases.GetAllLeaguesUseCase
+import com.kfadli.fdj.domain.usecases.GetTeamsByLeagueUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class HomeViewModel(
     private val getLeaguesUseCase: GetAllLeaguesUseCase,
+    private val getTeamsUseCase: GetTeamsByLeagueUseCase,
 ) : ViewModel() {
     val state = MutableStateFlow<UIState>(UIState.Loading)
 
     fun getLeagues() {
         Timber.v("[getLeagues]")
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             getLeaguesUseCase()
                 .onSuccess {
                     Timber.d("[getLeagues] Success")
-                    state.value = UIState.Success(it)
+                    state.value = UIState.LeagueUISuccess(items = it)
                 }.onFailure {
-                    Timber.w("[getLeagues] Failure", it)
-                    state.value = UIState.Failure(it)
+                    Timber.w(message = "[getLeagues] Failure", t = it)
+                    state.value = UIState.Failure(cause = it)
+                    delay(200)
+                    state.value = UIState.Idle
+                }
+        }
+    }
+
+    fun getTeams(leagueName: String) {
+        Timber.v("[getTeams] leagueName: $leagueName")
+        viewModelScope.launch(Dispatchers.IO) {
+            getTeamsUseCase(leagueName)
+                .onSuccess {
+                    Timber.d(message = "[getTeams] Success")
+                    state.value = UIState.TeamsUISuccess(items = it)
+                }.onFailure {
+                    Timber.w(message = "[getTeams] Failure", t = it)
+                    state.value = UIState.Failure(cause = it)
+                    delay(200)
+                    state.value = UIState.Idle
                 }
         }
     }
 }
 
 sealed class UIState {
+    data object Idle : UIState()
+
     data object Loading : UIState()
 
-    data class Success(
+    data class LeagueUISuccess(
         val items: List<LeagueUI>,
+    ) : UIState()
+
+    data class TeamsUISuccess(
+        val items: List<TeamUI>,
     ) : UIState()
 
     data class Failure(
