f614d7cfe7262213ab59672226806d79e36b9173
Karim <karimm.fadli@gmail.Com>
2024-08-26 18:09:09 +0200

    Add Mockk dependency for UnitTest

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- features/build.gradle.kts
+++ features/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
