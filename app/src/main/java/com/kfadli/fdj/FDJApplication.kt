ba0a3ceb26c592174c36e36fc9d1263e38521369
Karim <karimm.fadli@gmail.Com>
2024-08-26 17:19:15 +0200

    Configure Timber

--- app/src/main/java/com/kfadli/fdj/FDJApplication.kt
+++ app/src/main/java/com/kfadli/fdj/FDJApplication.kt
@@ -5,10 +5,13 @@ import com.kfadli.fdj.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import timber.log.Timber
 
 class FDJApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
 
         startKoin {
             androidLogger()
