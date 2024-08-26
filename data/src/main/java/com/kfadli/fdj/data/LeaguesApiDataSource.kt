17305bc5c03647fbc3e8667829688b41d3c10611
Karim <karimm.fadli@gmail.Com>
2024-08-26 17:16:57 +0200

    Implements ApiDatasource

--- data/src/main/java/com/kfadli/fdj/data/LeaguesApiDataSource.kt
+++ data/src/main/java/com/kfadli/fdj/data/LeaguesApiDataSource.kt
@@ -1,17 +1,47 @@
 package com.kfadli.fdj.data
 
+import com.kfadli.fdj.data.network.response.LeaguesResponse
+import com.kfadli.fdj.data.network.response.TeamsResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.encodeURLPath
 
 class LeaguesApiDataSource(
     private val clientApi: HttpClient,
 ) {
-    fun getLeague(leagueId: Long): Result<League> = Result.success(League(1L, "Champions League"))
+    suspend fun getTeamsByLeague(leagueName: String): Result<TeamsResponse> =
+        kotlin.runCatching {
+            clientApi.get<TeamsResponse>(url = SportsApi.AllTeams(query = leagueName.encodeURLPath()).url)
+        }
 
-    fun getAllLeagues(): Result<List<League>> =
-        Result.success(
-            listOf(
-                League(id = 1L, name = "Champions League"),
-                League(2L, "Europa League"),
-            ),
-        )
+    suspend fun getAllLeagues(): Result<LeaguesResponse> = runCatching { clientApi.get<LeaguesResponse>(url = SportsApi.AllLeagues.url) }
 }
+
+suspend inline fun <reified R> HttpClient.get(url: String): R =
+    get(url) {
+        contentType(ContentType.Application.Json)
+    }.body()
+
+suspend fun HttpClient.getOrNull(url: String): HttpResponse =
+    get(url) {
+        contentType(ContentType.Application.Json)
+    }
+
+sealed class SportsApi(
+    path: String,
+) {
+    data object AllLeagues : SportsApi("all_leagues.php")
+
+    data class AllTeams(
+        val query: String? = null,
+    ) : SportsApi("search_all_teams.php?l=$query")
+
+    val url = "$URL_API/$path"
+}
+
+private const val THE_SPORTS_DB_KEY = "50130162"
+private const val URL_API = "https://www.thesportsdb.com/api/v1/json/${THE_SPORTS_DB_KEY}"
