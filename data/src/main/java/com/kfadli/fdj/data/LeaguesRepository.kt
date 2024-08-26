6c99d508883a4cf13a42d5908067151eff076ee4
Karim <karimm.fadli@gmail.Com>
2024-08-26 17:17:09 +0200

    Implement LeagueRepository

--- data/src/main/java/com/kfadli/fdj/data/LeaguesRepository.kt
+++ data/src/main/java/com/kfadli/fdj/data/LeaguesRepository.kt
@@ -1,9 +1,26 @@
 package com.kfadli.fdj.data
 
+import com.kfadli.fdj.data.model.League
+import com.kfadli.fdj.data.model.Team
+import com.kfadli.fdj.data.network.response.LeagueResponse
+import com.kfadli.fdj.data.network.response.TeamResponse
+
 class LeaguesRepository(
     private val dataSource: LeaguesApiDataSource,
 ) {
-    suspend fun getLeague(leagueId: Long): Result<League> = dataSource.getLeague(leagueId)
+    suspend fun getAllLeagues(): Result<List<League>> = dataSource.getAllLeagues().map { it.leagues.toLeagues() }
 
-    suspend fun getAllLeagues(): Result<List<League>> = dataSource.getAllLeagues()
+    suspend fun getTeamsByLeague(leagueName: String): Result<List<Team>> =
+        dataSource.getTeamsByLeague(leagueName).map { it.teams?.toTeams() ?: emptyList() }
 }
+
+private fun List<TeamResponse>.toTeams(): List<Team> =
+    map { response ->
+        Team(
+            id = response.id.toLong(),
+            name = response.name,
+            badgeUrl = response.badge,
+        )
+    }
+
+private fun List<LeagueResponse>.toLeagues(): List<League> = map { response -> League(response.id.toLong(), response.name) }
